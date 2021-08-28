96fa79f0b4e9e9fc54fdb2ecbccb6dac2503bb0c
tamaroning <20992019+tamaroning@users.noreply.github.com>
2021-08-28 11:26:21 +0900

    Refactoring

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 CCR_DIR="$(cd $(dirname $0); pwd)/"
 DEBUG="${CCR_DIR}target/debug/"
 
-"${DEBUG}"ccr "$1"
+"${DEBUG}"ccr ${CCR_DIR}csrc/test.txt #"$1"
 cc "${CCR_DIR}"csrc/link.c "${DEBUG}"tmp.s -o "${DEBUG}"tmp
 "${DEBUG}"tmp
