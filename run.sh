e9c6011b3fa808f60e111b17f6a78d341ae3f5c0
tamaroning <20992019+tamaroning@users.noreply.github.com>
2021-08-28 14:25:32 +0900

    Clean

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 CCR_DIR="$(cd $(dirname $0); pwd)/"
 DEBUG="${CCR_DIR}target/debug/"
 
-"${DEBUG}"ccr ${CCR_DIR}csrc/test.txt #"$1"
-cc "${CCR_DIR}"csrc/link.c "${DEBUG}"tmp.s -o "${DEBUG}"tmp
+"${DEBUG}"ccr ${CCR_DIR}test/link-test/test.txt #"$1"
+cc "${CCR_DIR}"test/link-test/link.c "${DEBUG}"tmp.s -o "${DEBUG}"tmp
 "${DEBUG}"tmp
